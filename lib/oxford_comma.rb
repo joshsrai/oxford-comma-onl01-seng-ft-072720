78ad5ee8f42e177b38a2e551741328ba627f1313
Josh Raiborde <joshsrai@yahoo.com>
2020-07-28 19:18:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 def oxford_comma(array)
   if array.size == 2
     array.join(" and ")
-    
+    elsif 2 < array.size
+    conjuction = array.pop
+    new_list = array.join(", ")
+    new_list << ", and #{conjuction}"
+  else
+    array.join
+  end
 end
